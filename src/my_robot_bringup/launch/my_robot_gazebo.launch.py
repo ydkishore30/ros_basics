fd040ba99a77d4000450d1760099d83b3e8e542e
Dinesh Kishor <ydkishore30@gmail.com>
2026-04-17 10:14:36 +0530

    change directory of sample.pdf

--- src/my_robot_bringup/launch/my_robot_gazebo.launch.py
+++ src/my_robot_bringup/launch/my_robot_gazebo.launch.py
@@ -40,7 +40,7 @@ def generate_launch_description():
     # ------------------------------
     gazebo = ExecuteProcess(
         cmd=["gz", "sim", "-v", "4", "-r",
-             "/home/ubuntu/ros_basics/src/my_robot_description/worlds/sample.sdf"],
+             "/home/dinesh/ros_basics/src/my_robot_description/worlds/sample.sdf"],
         output="screen"
     )
 
